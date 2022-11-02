b8a49326dcd12b2bee06dce9c387718c4c4ae94d
Arun1092 <116347431+Arun1092@users.noreply.github.com>
2022-11-02 14:47:59 +0530

    Add files via upload

---
+++ greaterthan.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a number"))
+b=int (input("Enter the second number"))
+c=int(input("Enter the third number"))
+if a>b:
+    print(a, "is grater")
+elif b>c:
+    print(b, "is greater")
+else:
+    print(c,"is greater")
