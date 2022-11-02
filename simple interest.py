ea8c0b345dfb7fd114660c9f1939459285003be0
Arun1092 <116347431+Arun1092@users.noreply.github.com>
2022-11-02 14:50:27 +0530

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,8 @@
+p=int(input("Enter the principal amount"))
+r=int(input("Enter the rathe of interest"))
+t=int(input("Ente the time taken" ))
+si=(p*t*r)/100
+print("principal amount ",p)
+print("rate of interest",r)
+print("time taken",t)
+print("simple interest is",si)
