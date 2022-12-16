7459442c9c8cc63055d56794f5c58a4ac32e0f89
Arun Kumar AG <116347431+Arun1092@users.noreply.github.com>
2022-12-16 15:27:00 +0530

    Add files via upload

---
+++ largest_of_numbers.py
@@ -0,0 +1,14 @@
+"//find the largest of 10 number in  python?//"
+
+ages =()
+while True:
+    next_val = int(input())
+    if nex_val == -1:
+        break
+    ages.append(next_val)
+ages.sort()
+print(ages[-2:])
+
+
+
+
